db4eee9365242bdd62dbf99e120f32c15e02035d
ashwath-karthikeyan <ashwath.karthikeyan@gmail.com>
2024-07-07 15:32:23 -0400

    works, bounding boxes data got

--- pt_detect.py
+++ pt_detect.py
@@ -1,10 +1,8 @@
 from ultralytics import YOLO
 import cv2
-import numpy as np
-import torch
 
 # Paths to the model weights and the image
-MODEL_WEIGHT_PATH = '1/weights.onnx'
+MODEL_WEIGHT_PATH = 'weights.onnx'
 IMAGE_PATH = 'image.jpg'
 # IMAGE_PATH = 'box.jpg'
 
@@ -13,18 +11,9 @@ model = YOLO(MODEL_WEIGHT_PATH, task='detect')
 
 # Load and preprocess the image using cv2
 image = cv2.imread(IMAGE_PATH)
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-resized_image = cv2.resize(image_rgb, (640, 640))  # Resize image to match model's expected input size
-input_image = np.transpose(resized_image, (2, 0, 1))  # Change data layout from HWC to CHW
-input_image = input_image.astype(np.float32) / 255.0  # Normalize the image to [0, 1]
-input_image = np.expand_dims(input_image, axis=0)  # Add batch dimension
-
-# Convert to PyTorch tensor
-input_tensor = torch.tensor(input_image)
 
 # Run the model
-with torch.no_grad():
-    output = model(input_tensor)
+output = model(image)
 
 # Print the model's output
-print(output)
+boxes = (output[0].boxes)
